c3a0c7e547ab0f039f96d065d370ab3c0eb6aadd
Sergey Popov <quolpr@gmail.com>
2022-06-20 21:36:24 +0300

    Fix eslint

--- packages/absurd-web-backend/.eslintrc.js
+++ packages/absurd-web-backend/.eslintrc.js
@@ -3,7 +3,7 @@ const rootDir = process.cwd().includes("packages/absurd-web-backend")
   : "./packages/absurd-web-backend";
 
 module.exports = {
-  extends: ["../common-scripts/eslintrc"],
+  extends: ["../common-scripts/eslintrc.cjs"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
--- packages/vite-react-example/.eslintrc.js
+++ packages/vite-react-example/.eslintrc.js
@@ -3,7 +3,7 @@ const rootDir = process.cwd().includes("packages/vite-react-example")
   : "./packages/vite-react-example";
 
 module.exports = {
-  extends: ["../common-scripts/eslintrc"],
+  extends: ["../common-scripts/eslintrc.cjs"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
