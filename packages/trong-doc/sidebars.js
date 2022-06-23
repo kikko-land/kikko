76aafa3b932a8161cb1f295107c8cbd0a4d51860
Sergey Popov <quolpr@gmail.com>
2022-06-23 13:49:48 +0300

    Add useQuery example

--- packages/trong-doc/sidebars.js
+++ packages/trong-doc/sidebars.js
@@ -16,6 +16,7 @@ const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
   docs: [
     "intro",
+    "howReactiveQueriesWorks",
     "transactions",
     {
       type: "category",
