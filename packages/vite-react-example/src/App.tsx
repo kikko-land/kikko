bfb8e35901f0c7dd9eb54ff2e605dae821d5de5b
Sergey Popov <quolpr@gmail.com>
2022-07-11 15:05:31 +0300

    Use absurd in example

--- packages/vite-react-example/src/App.tsx
+++ packages/vite-react-example/src/App.tsx
@@ -9,20 +9,20 @@ import {
   reactiveQueriesPlugin,
 } from "@trong-orm/react";
 import absurdSqlWasmUrl from "@trong-orm/sql.js/dist/sql-wasm.wasm?url";
-import { waSqliteWebBackend } from "@trong-orm/wa-sqlite-web-backend";
-import sqlWasmUrl from "wa-sqlite/dist/wa-sqlite-async.wasm?url";
 
+// import { waSqliteWebBackend } from "@trong-orm/wa-sqlite-web-backend";
+// import sqlWasmUrl from "wa-sqlite/dist/wa-sqlite-async.wasm?url";
 import { List } from "./List";
 import { createNotesTableMigration } from "./migrations/createNotesTable";
 
 const config: IInitDbClientConfig = {
   dbName: "helloWorld",
-  // dbBackend: absurdWebBackend({
-  //   wasmUrl: absurdSqlWasmUrl,
-  //   pageSize: 32 * 1024,
-  //   cacheSize: -5000,
-  // }),
-  dbBackend: waSqliteWebBackend({ wasmUrl: sqlWasmUrl }),
+  dbBackend: absurdWebBackend({
+    wasmUrl: absurdSqlWasmUrl,
+    pageSize: 32 * 1024,
+    cacheSize: -5000,
+  }),
+  // dbBackend: waSqliteWebBackend({ wasmUrl: sqlWasmUrl }),
   plugins: [
     migrationsPlugin({ migrations: [createNotesTableMigration] }),
     reactiveQueriesPlugin(),
