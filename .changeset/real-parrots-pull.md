153570c16cfa166453bde003b5ae31235a4276d6
Sergey Popov <quolpr@gmail.com>
2022-10-27 00:31:44 +0300

    Add boono-sql reexport to react package

---
+++ .changeset/real-parrots-pull.md
@@ -0,0 +1,5 @@
+---
+"@kikko-land/react": minor
+---
+
+Add boono-sql reexport to react package
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
+export * from "@kikko-land/boono-sql";
 export * from "@kikko-land/kikko";
 export * from "@kikko-land/migrations-plugin";
 export * from "@kikko-land/react-queries-hooks";
